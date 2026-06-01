2a8521d4847a740219626c65b777a2ec75370c8a
Aca Asmarani <nadacantika330@gmail.com>
2026-06-01 13:07:07 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,5 +9,13 @@ export default defineConfig({
   server: {
     host: true,
     allowedHosts: ['.ngrok-free.app', 'unclaimed-unfreeze-editor.ngrok-free.dev'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
   },
 })
