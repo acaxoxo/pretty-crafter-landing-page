f418c54501cf0e2eac94349714e3b3482c290504
Aca Asmarani <nadacantika330@gmail.com>
2026-06-02 23:57:47 +0800

    again

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
-import serverless from 'serverless-http';
 import app from './server/app.js';
 
-export const config = {
-  api: { bodyParser: false }
-};
-
-export default serverless(app);
+export default app;
